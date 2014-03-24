4e754544d74e7afdf38b531c1bd62fd784f33ed7
jinger7281 <jinger7281@gmail.com>
2014-03-24 16:36:39 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+#encoding:utf-8
+
+import socket
+import slaves
+
+HOSTNAME='localhost'
+HOSTPORT='8052'
+SERVERADDR=(HOSTNAME,HOSTPORT)
+
+serverSocket=socket(socket.AF_INET,socket.SOCK_STREAM)
+serverSocket.bind(SERVERADDR)
+serverSocket.listen(0)
+clientStatistics=1
+while True:
+    clientSocket,clientAddr=Serversocket.accept()
+    clientStatistic+=1
+    print("*Client[",clientAddr[0],":",clientAddr[1],"]:",clientStatistic)
+    slave=slaves.Manager()
+    slave.setClientInfo(clientSocket,clientAddr)
